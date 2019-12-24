4af1dbff08626315ed9d9500193afe39fadfd3dc
Salman Khan <salmank@nxn.com.pk>
2019-12-24 15:45:50 +0500

    3rd commit

--- routes/poll.js
+++ routes/poll.js
@@ -19,27 +19,20 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  axios.get('http://payments-uat.nxn.com.pk/publish_mw_api/api/dashboard/get_dashboard_summary').then(response => {
+  const newVote = {
+    os: req.body.os,
+    points: 1
+  };
 
-    const [{ model_name, model_sold }] = response.data;
-    console.log(model_name);
-    console.log(model_sold);
-    const newVote = {
-      // os: req.body.os,
-      // points: 1
-      os: model_name,
-      points: model_sold
-    };
-
-    new Vote(newVote).save().then(vote => {
-      pusher.trigger('os-poll', 'os-vote', {
-        points: parseInt(vote.points),
-        os: vote.os
-      });
-      return res.json({ success: true, message: 'Thank you for voting' });
+  new Vote(newVote).save().then(vote => {
+    pusher.trigger('os-poll', 'os-vote', {
+      points: parseInt(vote.points),
+      os: vote.os
     });
-  }).catch(err => res.status(404).json(err));;
-})
+
+    return res.json({ success: true, message: 'Thank you for voting' });
+  });
+});
 
 
 
